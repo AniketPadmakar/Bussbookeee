7554aa438be19709fc5521d1c63751b99ca81cde
Aniket <66579134+AniketPadmakar@users.noreply.github.com>
2025-03-02 00:30:00 +0530

    Update URL.js

--- Bus_Develope_Frontend/src/data/URL.js
+++ Bus_Develope_Frontend/src/data/URL.js
@@ -1,14 +1,14 @@
-// const hostURL = {
-//     link:
-//       process.env.NODE_ENV === "production"
-//         ? "https://bussbookeee.onrender.com"
-//         : "http://localhost:4000",
-//   };
-  
-//   export default hostURL;
-  const hostURL = {
-    link:"http://localhost:4000"
+const hostURL = {
+    link:
+      process.env.NODE_ENV === "production"
+        ? "https://bussbookeee.onrender.com"
+        : "http://localhost:4000",
   };
   
   export default hostURL;
+  // const hostURL = {
+  //   link:"http://localhost:4000"
+  // };
+  
+  // export default hostURL;
   
